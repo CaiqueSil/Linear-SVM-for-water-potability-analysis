11b6e3665275aae55c2ae3c1ebc07edd520ae03e
Caique Silva Santos <caiquesilva.si2023@gmail.com>
2025-10-05 16:31:38 -0300

    Create preprocess.py

---
+++ src/preprocess.py
@@ -0,0 +1,11 @@
+# src/preprocess.py
+import pandas as pd
+from src.config import FEATURE_COLUMNS, TARGET_COLUMN
+
+def get_features_target(df: pd.DataFrame) -> tuple[pd.DataFrame, pd.Series]:
+    """
+    Extrai as features (X) e o target (y) do DataFrame.
+    """
+    X = df[FEATURE_COLUMNS].copy()
+    y = df[TARGET_COLUMN].copy()
+    return X, y
